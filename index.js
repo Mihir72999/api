3bb7e8ec4c26bac8b25528233a01d7d5ba58533e
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2023-05-25 23:10:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 const port =process.env.PORT || 4000
 const app = express()
-app.use(cors())
+
 app.use(express.json())
+app.use(cors())
 
 app.use(cookieParser())
 connectMongoDb()
